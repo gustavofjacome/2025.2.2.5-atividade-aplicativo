354c2c82428801592ed6cd2b386bbb856e237c8b
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-02-07 17:06:36 -0300

    add constantes do js

---
+++ script.js
@@ -0,0 +1,6 @@
+// const que vao ser manipuladas pelo js
+const cepInput = document.getElementById('cepInput');
+const buscarBtn = document.getElementById('buscarBtn');
+const limparBtn = document.getElementById('limparBtn');
+const resultado = document.getElementById('resultado');
+const lista = document.getElementById('lista');
