2917a9902dc6b43ce56fc0f2cb0b209202e1c83e
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-02-07 17:07:05 -0300

    função que busca os dados pela api

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ const buscarBtn = document.getElementById('buscarBtn');
 const limparBtn = document.getElementById('limparBtn');
 const resultado = document.getElementById('resultado');
 const lista = document.getElementById('lista');
+
+// puxando da api
+async function buscarCEP() {
+    const cep = cepInput.value.replace(/\D/g, '');
+    
+    if (cep.length !== 8) {
+        alert('Digite um CEP válido com 8 números!');
+        return;
+    }
+    
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const dados = await response.json();
+        
+        if (dados.erro) {
+            alert('CEP não encontrado!');
+            return;
+        }
+        
+        mostrarResultado(dados);
+        salvarNoHistorico(dados);
+        
+    } catch (erro) {
+        alert('Erro ao buscar CEP. Tente novamente.');
+    }
+}
+
